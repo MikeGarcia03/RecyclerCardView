e590cdb45c464da0a97749b9b8be619d62b05b42
MIGUEL ANGEL GARCIA DESENA <mg2013-0864@UCATEBA.EDU.DO>
2018-08-24 17:56:29 -0400

    Initial commit

---
+++ app/src/main/java/com/example/mg2013_0864/recyclercardview/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mg2013_0864.recyclercardview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.header_layout);
+    }
+}
